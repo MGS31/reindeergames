fcef848b8d05d4ef9340d9bbe51b2912dcfaaa40
Matt <mgstankov30@gmail.com>
2023-09-03 13:26:56 -0400

    add form for users to add their names, Add ability to dynamically add more names, add the ability to remove names and submit the information

--- src/components/Pages/List.js
+++ src/components/Pages/List.js
@@ -1,7 +1,85 @@
-import React from 'react';
+import { useState, useRef } from 'react';
+import Input from '../../constants/Input';
 
 const List = () => {
-  return <div></div>;
+  const [formValues, setFormValues] = useState([
+    {
+      label: 'Name',
+      type: 'text',
+      value: '',
+    },
+  ]);
+  const [toggle, setToggle] = useState(false);
+  const inputRef = useRef();
+  const selectRef = useRef();
+
+  const handleChange = (e, index) => {
+    const values = [...formValues];
+    values[index].value = e.target.value;
+    setFormValues(values);
+  };
+
+  const handleAddField = (e) => {
+    e.preventDefault();
+    const values = [...formValues];
+    values.push({
+      label: inputRef.current.value || 'Name',
+      type: 'text',
+      value: inputRef.current.value || '',
+    });
+    setFormValues(values);
+    setToggle(false);
+  };
+
+  const handleDeleteField = (e, index) => {
+    const values = [...formValues];
+    values.splice(index, 1);
+    setFormValues(values);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    sessionStorage.setItem('Reindeer-Names', formValues);
+  };
+
+  const addBtnClick = (e) => {
+    e.preventDefault();
+    setToggle(true);
+  };
+
+  return (
+    <div>
+      <h1>Enter Reindeer Here:</h1>
+      <form onSubmit={handleSubmit}>
+        {formValues.map((obj, index) => (
+          <Input
+            key={index}
+            objValue={obj}
+            onChange={handleChange}
+            index={index}
+            deleteField={handleDeleteField}
+          />
+        ))}
+        {!toggle ? (
+          <div className="center">
+            <button className="add-btn" onClick={addBtnClick}>
+              Add Name
+            </button>
+          </div>
+        ) : (
+          <div className="dialog-box">
+            <input type="text" placeholder="Enter Name" ref={inputRef} />
+            <button className="add-btn" onClick={handleAddField}>
+              Add
+            </button>
+          </div>
+        )}
+        <button type="submit" className="submit-btn">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default List;
