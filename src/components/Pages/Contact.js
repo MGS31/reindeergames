7917841784bb32990ea6e6458a2cd0941610da4b
Matt <mgstankov30@gmail.com>
2023-09-03 12:43:26 -0400

    add basic fake contact information for the contact page

---
+++ src/components/Pages/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h3>Contact</h3>
+      <p>For any questions concerns of feedback please contact us here:</p>
+      <ul>
+        <li>Email: ReindeerGames@gmail.com</li>
+        <li>Phone: 1-800-ReindeerGames</li>
+        <li>Mail: 1 The North Pole</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Contact;
