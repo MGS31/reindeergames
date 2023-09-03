478953948bfede8fb3ea103cac7039c0fc481c8b
Matt <mgstankov30@gmail.com>
2023-09-03 12:29:17 -0400

    add router information and detailed routes as well as import Nav board for the main layout of the page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import Nav from './components/Nav';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './components/Pages/Home';
+import Rules from './components/Pages/Rules';
+import List from './components/Pages/List';
+import Contact from './components/Pages/Contact';
 
 const App = () => {
-  return <div></div>;
+  return (
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path="/" Component={Home} />
+        <Route path="/Rules" Component={Rules} />
+        <Route path="/List" Component={List} />
+        <Route path="/Contact" Component={Contact} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
