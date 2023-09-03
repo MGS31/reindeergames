d6e36d26afa2cc3a3efec476e1c783d946fbc629
Matt <mgstankov30@gmail.com>
2023-09-03 12:28:31 -0400

    Add basic details to the home page for details on the web app

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="text-box">
+        <h3>Welcome to ReindeerGames!</h3>
+        <p>The easiest way to get your holiday games started</p>
+        <p>
+          If you're looking for a quick way to get your Secret Santa, White
+          Elephant or any other group based games started
+        </p>
+        <p>Look no further than Reindeer Games! Click Below to get started!</p>
+        <h5>Lets Get Started</h5>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
