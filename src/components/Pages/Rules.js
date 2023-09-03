9b418368b30ebd90332cffd3810248e1047833d4
Matt <mgstankov30@gmail.com>
2023-09-03 12:40:22 -0400

    add a basic first draft of the rules info for later styling and position on the rules page

--- src/components/Pages/Rules.js
+++ src/components/Pages/Rules.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
 const Rules = () => {
-  return <div></div>;
+  return (
+    <div className="rules">
+      <h1 className="rules-header">Rules</h1>
+      <p>
+        The idea is simple, you want to have everyone participate in a fun gift
+        giving exchange.
+      </p>
+      <p>
+        BUT you want to make sure it's random and fair for everyone involved. No
+        favourites on Christmas!
+      </p>
+      <p>
+        Reindeer games will take all members of the group and mix them together
+        before returning a list of pairs.
+      </p>
+      <p>from there you can set a budget or spending limit and a </p>
+    </div>
+  );
 };
 
 export default Rules;
