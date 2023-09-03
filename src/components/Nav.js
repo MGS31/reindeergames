663bd81a183b50e1ecb9075484be89416ecf1f8e
Matt <mgstankov30@gmail.com>
2023-09-03 12:29:40 -0400

    add Links in the Nav page for the different pages of the nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
-  return <div></div>;
+  return (
+    <header className="top">
+      <img src={''} alt="Logo" />
+      <h1>
+        Reindeer
+        <span className="Games">
+          <span>Games</span>
+        </span>
+      </h1>
+      <span className="links">
+        <span className="Home">
+          <Link to="/">Home</Link>
+        </span>
+        <span className="Rules">
+          <Link to="/Rules">Rules</Link>
+        </span>
+        <span className="create">
+          <Link to="/List">Create Group</Link>
+        </span>
+        <span className="contact">
+          <Link to="/Contact">Contact</Link>
+        </span>
+      </span>
+    </header>
+  );
 };
 
 export default Nav;
