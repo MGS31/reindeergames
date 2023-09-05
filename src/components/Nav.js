8c39261df4c11ee26911769d1f19a221ce9d8c45
Matt <mgstankov30@gmail.com>
2023-09-05 19:44:32 -0400

    update layout of nav DIV's for styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,29 +7,31 @@ const Nav = () => {
   return (
     <header className="top">
       <div className="logo-nav">
-        <img src={''} alt="Logo" />
-        <h1>
-          Reindeer
-          <span className="Games">
-            <span>Games</span>
-          </span>
-        </h1>
+        <div className="logo-image">
+          <img src={''} alt="Logo" />
+        </div>
+        <div className="app-title">
+          <h1>
+            Reindeer
+            <span className="Games">
+              <span>Games</span>
+            </span>
+          </h1>
+        </div>
       </div>
       <div className="link-nav">
-        <span className="links">
-          <span className="Home">
-            <Link to="/">Home</Link>
-          </span>
-          <span className="Rules">
-            <Link to="/Rules">Rules</Link>
-          </span>
-          <span className="create">
-            <Link to="/List">Create Group</Link>
-          </span>
-          <span className="contact">
-            <Link to="/Contact">Contact</Link>
-          </span>
-        </span>
+        <div className="Home">
+          <Link to="/"> Home </Link>
+        </div>
+        <div className="Rules">
+          <Link to="/Rules"> Rules </Link>
+        </div>
+        <div className="create">
+          <Link to="/List"> Create Group </Link>
+        </div>
+        <div className="contact">
+          <Link to="/Contact"> Contact </Link>
+        </div>
       </div>
     </header>
   );
