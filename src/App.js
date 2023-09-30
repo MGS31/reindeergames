de774272536b5410ac0737bac03588b90a6683e9
Matt <mgstankov30@gmail.com>
2023-09-30 15:39:31 -0400

    add Particles for snow to fall

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Particles from 'react-tsparticles';
+import { loadFull } from 'tsparticles';
 
 import Nav from './components/Nav';
 import Home from './components/Pages/Home';
@@ -8,18 +10,54 @@ import List from './components/Pages/List';
 import Contact from './components/Pages/Contact';
 import Results from './components/Pages/Results';
 
+import '../src/styles/style.scss';
+
 const App = () => {
+  const [count, setCount] = useState(50);
+  const init = useCallback(async (engine) => {
+    await loadFull(engine);
+  });
   return (
-    <Router>
-      <Nav />
-      <Routes>
-        <Route path="/" Component={Home} />
-        <Route path="/Rules" Component={Rules} />
-        <Route path="/List" Component={List} />
-        <Route path="/Contact" Component={Contact} />
-        <Route path="/Results" Component={Results} />
-      </Routes>
-    </Router>
+    <>
+      <Particles
+        options={{
+          particles: {
+            color: {
+              value: '#fff',
+            },
+            number: {
+              value: count,
+            },
+            opacity: {
+              value: { min: 0.3, max: 1 },
+            },
+            shape: {
+              type: 'circle',
+            },
+            size: {
+              value: { min: 0.3, max: 5 },
+            },
+            move: {
+              direction: 'bottom',
+              enable: true,
+              speed: { min: 2, max: 5 },
+              straight: true,
+            },
+          },
+        }}
+        init={init}
+      />
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" Component={Home} />
+          <Route path="/Rules" Component={Rules} />
+          <Route path="/List" Component={List} />
+          <Route path="/Contact" Component={Contact} />
+          <Route path="/Results" Component={Results} />
+        </Routes>
+      </Router>
+    </>
   );
 };
 
