4c2564e57d00a5365719f86cd34923b5a8053121
Matt <mgstankov30@gmail.com>
2023-09-30 15:40:49 -0400

    refactor html for better styling and layout

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -6,50 +6,46 @@ import '../../styles/home.scss';
 
 const Home = () => {
   return (
-    <>
-      <div className="home-box">
-        <div className="text-box">
-          <h2>Welcome to ReindeerGames!</h2>
-          <p>The easiest way to get your holiday games started</p>
-          <p>
-            If you're looking for a quick way to get your Secret Santa, <br />
-            <br />
-            White Elephant or any other group based games started
-          </p>
-          <p>
-            Look no further than Reindeer Games! Click Below to get started!
-          </p>
-          <h4>
-            <Link className="get-started-btn" to="/List">
-              Lets Get Started
-            </Link>
-          </h4>
-        </div>
-        <div className="loop">
-          <img
-            className="green-tree"
-            alt="green-tree"
-            src="../images/GreenTree.png"
-            width={465}
-            height={465}
-          />
-          <img
-            className="red-tree"
-            alt="red-tree"
-            src="../images/RedTree.png"
-            width={465}
-            height={465}
-          />
-          <img
-            className="purple-tree"
-            alt="purple-tree"
-            src="../images/PurpleTree.png"
-            width={465}
-            height={465}
-          />
-        </div>
+    <div className="home-box">
+      <div className="text-box">
+        <h2>Welcome to ReindeerGames!</h2>
+        <p>The easiest way to get your holiday games started</p>
+        <p>
+          If you're looking for a quick way to get your Secret Santa, <br />
+          <br />
+          White Elephant or any other group based games started
+        </p>
+        <p>Look no further than Reindeer Games! Click Below to get started!</p>
+        <h4>
+          <Link className="get-started-btn" to="/List">
+            Lets Get Started
+          </Link>
+        </h4>
       </div>
-    </>
+      <div className="loop">
+        <img
+          className="green-tree"
+          alt="green-tree"
+          src="../images/GreenTree.png"
+          width={465}
+          height={465}
+        />
+        <img
+          className="red-tree"
+          alt="red-tree"
+          src="../images/RedTree.png"
+          width={465}
+          height={465}
+        />
+        <img
+          className="purple-tree"
+          alt="purple-tree"
+          src="../images/PurpleTree.png"
+          width={465}
+          height={465}
+        />
+      </div>
+    </div>
   );
 };
 
